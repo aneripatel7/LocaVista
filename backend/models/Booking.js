07b075b567531b5f422b56fe7277ddb014bfbdd9
aneripatel78 <aneriipatell78@gmail.com>
2025-03-04 17:49:07 +0530

    final

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
 
 const BookingSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "Attendee", required: true },
     eventId: { type: mongoose.Schema.Types.ObjectId, ref: "Event", required: true },
+    ticketId: { type: String, unique: true, default: uuidv4 },
+    qrCodeUrl: { type: String },  // Store QR code URL
     bookedAt: { type: Date, default: Date.now }
 });
 
